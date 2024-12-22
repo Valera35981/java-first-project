37bc2a288243f4c0dd6ec5f71522f30d755407c1
Valerii <pc@DESKTOP-B732DOH.(none)>
2024-12-23 01:53:18 +0300

    Добавлен код для нахождения максимума из четырех чисел

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5, b = 10, c = 3, d = 8;
+        int max = Math.max(Math.max(a, b), Math.max(c, d));
+        System.out.println("Максимум: " + max);
+    }
+}
+     
